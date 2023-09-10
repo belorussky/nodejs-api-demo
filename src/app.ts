e0676c6997ab59d94bd58a85826aca1d08de7f36
belorussky <vkp2010@yandex.ru>
2023-09-10 22:31:20 +0200

    Exeption Filter and Users Controller

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,40 @@
 import express, { Express } from 'express';
-import { userRouter } from './users/users';
 import { Server } from 'http';
 import { LoggerService } from './logger/logger.service';
+import { UserController } from './users/users.controller';
+import { ExeptionFilter } from './errors/exeption.filter';
 
 export class App {
     app: Express;
     server: Server;
     port: number;
-    logger: LoggerService
+    logger: LoggerService;
+    userController: UserController;
+    exeptionFilter: ExeptionFilter;
 ;
-    constructor(logger: LoggerService) {
+    constructor(
+        logger: LoggerService,
+        userController: UserController,
+        exeptionFilter: ExeptionFilter
+        ) {
         this.app = express();
         this.port = 8000;
         this.logger = logger;
+        this.userController = userController;
+        this.exeptionFilter = exeptionFilter;
     }
 
     useRouts() {
-        this.app.use('/users', userRouter);
+        this.app.use('/users', this.userController.router);
+    }
+
+    useExeptionFilters() {
+        this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
     }
 
     public async init() {
         this.useRouts();
+        this.useExeptionFilters();
         this.server = this.app.listen(this.port);
         this.logger.log(`The server was started on http://localhost:${this.port}`);
     }
