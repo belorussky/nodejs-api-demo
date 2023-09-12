64360bc48c90a1de8c5693f3c34541b2e0de1589
belorussky <vkp2010@yandex.ru>
2023-09-12 22:09:43 +0200

    Dependency Injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import { BaseController } from "../common/baseController";
-import { LoggerService } from "../logger/logger.service";
 import { HTTPError } from "../errors/http-error.class";
+import { inject, injectable } from "inversify";
+import { ILogger } from "../logger/logger.interface";
+import { TYPES } from "../types";
+import 'reflect-metadata';
+import { IUserController } from "./users.controller.interface";
 
-export class UserController extends BaseController {
+@injectable()
+export class UserController extends BaseController implements IUserController {
     constructor(
-        logger: LoggerService
+        @inject(TYPES.ILogger) private loggerService: ILogger
         ) {
-        super(logger);
+        super(loggerService);
         this.bindRoutes([
             { path: '/register', method: 'post', func: this.register },
             { path: '/login', method: 'post', func: this.login },
