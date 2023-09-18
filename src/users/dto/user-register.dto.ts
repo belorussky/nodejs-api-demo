93f8badda33aa94b1178d70e007e8ac03aa6b9f8
belorussky <vkp2010@yandex.ru>
2023-09-18 21:02:28 +0200

    Data Validation

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsString, IsEmail } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'The email address is specified incorrectly' })
 	email: string;
+
+	@IsString({ message: 'No password specified' })
 	password: string;
+
+	@IsString({ message: 'No name specified' })
 	name: string;
 }
