37ed188c62dbefc4ab4c65b44e3905ace6d81146
belorussky <vkp2010@yandex.ru>
2023-09-19 15:07:38 +0200

    User Repository and Login Method

--- src/users/dto/user-login.dto.ts
+++ src/users/dto/user-login.dto.ts
@@ -1,4 +1,9 @@
+import { IsString, IsEmail } from 'class-validator';
+
 export class UserLoginDto {
+	@IsEmail({}, { message: 'The email address is specified incorrectly' })
 	email: string;
+
+	@IsString({ message: 'No password specified' })
 	password: string;
 }
